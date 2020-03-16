b0d1f6a6b1a57908cf0a810a8d400da0f3de957b
spyspy99a88b <8270952@qq.com>
2020-03-16 16:16:55 -0400

    finsh the game

--- Assets/decode_hacker.cs
+++ Assets/decode_hacker.cs
@@ -8,6 +8,8 @@ public class decode_hacker : MonoBehaviour
     int level;
     enum Screen { MainMenu, Password, Win }; 
     Screen currentScreen;
+    const string menuHint = "You may type menu at any time.";
+
     string password;
     // Decoder information
     string[] level1Passwords = { "books", "aisle", "shelf", "password", "font", "borrow" };
@@ -53,23 +55,20 @@ public class decode_hacker : MonoBehaviour
     }
     void MainMenu(string input)
     {
-        if(input == "1"){
-            level=1;
-            password=level1Passwords;
-            StartGame();
+        bool isValidLevelNumber = (input == "1" || input == "2" || input == "3");
+        if (isValidLevelNumber)
+        {
+            level = int.Parse(input);
+            AskForPassword();
         }
-        else if (input == "2"){
-            level=2;
-            password=level2Passwords;
-            StartGame();
+        else if (input == "007") // easter egg
+        {
+            Terminal.WriteLine("Please select a level Mr Bond!");
         }
-        else if (input == "3"){
-            level=3;
-            password=level3Passwords;
-            StartGame();
-        }
-        else{
-
+        else
+        {
+            Terminal.WriteLine("Please choose a valid level");
+            Terminal.WriteLine(menuHint);
         }
     }
 
@@ -81,13 +80,92 @@ public class decode_hacker : MonoBehaviour
     }
     void CheckPassword(string input)
     {
-        if(input==password)
+        if (input == password)
         {
-            Terminal.WriteLine("Good job!");
+            DisplayWinScreen();
         }
         else
         {
-           Terminal.WriteLine("Wrong password!"); 
+            AskForPassword();
+        }
+    }
+
+    void AskForPassword()
+    {
+        currentScreen = Screen.Password;
+        Terminal.ClearScreen();
+        SetRandomPassword();
+        Terminal.WriteLine("Enter your password.");
+        Terminal.WriteLine("hint:"+ password.Anagram());
+        Terminal.WriteLine(menuHint);
+    }
+    void SetRandomPassword()
+    {
+        switch (level)
+        {
+            case 1:
+                password = level1Passwords[Random.Range(0, level1Passwords.Length)];
+                break;
+            case 2:
+                password = level2Passwords[Random.Range(0, level2Passwords.Length)];
+                break;
+            case 3:
+                password = level3Passwords[Random.Range(0, level3Passwords.Length)];
+                break;
+            default:
+                Debug.LogError("Invalid level number");
+                break;
+        }
+    }
+    void DisplayWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+        Terminal.WriteLine(menuHint);
+    }
+
+    void ShowLevelReward()
+    {
+        switch (level)
+        {
+            case 1:
+                Terminal.WriteLine("Have a book...");
+                Terminal.WriteLine(@"
+    _______
+   /      //
+  /      //
+ /_____ //
+(______(/           
+"
+                );
+                break;
+            case 2:
+                Terminal.WriteLine("You got the prison key!");
+                Terminal.WriteLine("Play again for a greater challenge.");
+                Terminal.WriteLine(@"
+ __
+/0 \_______
+\__/-=' = '         
+"
+                );
+                break;
+            case 3:
+                Terminal.WriteLine(@"
+ _ __   __ _ ___  __ _
+| '_ \ / _` / __|/ _` |
+| | | | (_| \__ \ (_| |
+|_| |_|\__,_|___)\__,_|
+
+
+
+"
+                );
+                Terminal.WriteLine("Welcome to NASA's internal system!");
+                break;
+            default:
+                Debug.LogError("Invalid level reached");
+                break;
         }
     }
 }
