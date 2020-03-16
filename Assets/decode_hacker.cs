0624ca62d05ba3e87a12df92194c42e1ba62beac
spyspy99a88b <8270952@qq.com>
2020-03-16 02:07:23 -0400

    update decode function

--- Assets/decode_hacker.cs
+++ Assets/decode_hacker.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class decode_hacker : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Game State
+    int level;
+    enum Screen { MainMenu, Password, Win }; 
+    Screen currentScreen;
+    string password;
+    // Decoder information
+    string[] level1Passwords = { "books", "aisle", "shelf", "password", "font", "borrow" };
+    string[] level2Passwords = { "prisoner", "handcuffs", "holster", "uniform", "arrest" };
+    string[] level3Passwords = { "starfield", "telescope", "environment", "exploration", "astronauts" };
+
     void Start()
     {
         ShowMainMenu();
     }
 
-    // Update is called once per frame
-    void ShowMainMenu(){
+    void ShowMainMenu()
+    {
+        currentScreen=Screen.MainMenu;
+        Terminal.ClearScreen();
         Terminal.WriteLine("It is a decode game. Choose a category by typing 1/2/3.");
-        Terminal.WriteLine("1:");
-        Terminal.WriteLine("2:");
+        Terminal.WriteLine("Press 1 for Animals");
+        Terminal.WriteLine("Press 2 for Devices");
+        Terminal.WriteLine("Press 3 for Terminology");
+        Terminal.WriteLine("Your selection is:");
     }
-    void Update()
-    {
-        
-    }
-    void OnUserInput(string input)
-    {
-        if (input == "menu") // we can always go direct to main menu
+    
+    // get directly to thd 3 main screen
+    void OnUserInput(string input) 
+    { 
+        if (input == "menu") 
         {
             ShowMainMenu();
         }
@@ -31,13 +42,52 @@ public class decode_hacker : MonoBehaviour
             Terminal.WriteLine("If on the web close the tab.");
             Application.Quit();
         }
-        // else if (currentScreen == Screen.MainMenu)
-        // {
-        //     RunMainMenu(input);
-        // }
-        // else if (currentScreen == Screen.Password)
-        // {
-        //     CheckPassword(input);
-        // }
+        else if (currentScreen == Screen.MainMenu)
+        {
+            MainMenu(input);
+        }
+         else if (currentScreen == Screen.Password)
+         {
+             CheckPassword(input);
+         }
+    }
+    void MainMenu(string input)
+    {
+        if(input == "1"){
+            level=1;
+            password=level1Passwords;
+            StartGame();
+        }
+        else if (input == "2"){
+            level=2;
+            password=level2Passwords;
+            StartGame();
+        }
+        else if (input == "3"){
+            level=3;
+            password=level3Passwords;
+            StartGame();
+        }
+        else{
+
+        }
+    }
+
+    void StartGame()
+    {
+        currentScreen=Screen.Password;
+        Terminal.WriteLine("The current level is: "+level);
+        Terminal.WriteLine("Enter the password:");
+    }
+    void CheckPassword(string input)
+    {
+        if(input==password)
+        {
+            Terminal.WriteLine("Good job!");
+        }
+        else
+        {
+           Terminal.WriteLine("Wrong password!"); 
+        }
     }
 }
